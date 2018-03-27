9eda175f4a13651e01b2fe953cfaee654192b304
LongingWorld <shadowzzj@126.com>
2018-03-28 00:26:48 +0800

    var go

--- basic.go
+++ basic.go
@@ -2,6 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/cmplx"
+	//"math"
+	//"math/cmplx"
 )
+
+var (
+	aa = 8
+	ss = "love"
+	kk = true
+)
+
+func variableZeroValue() {
+	a, b, c, s := 2, 3, 4, "abc"
+
+	fmt.Println(a, b, c, s)
+}
+
+func main() {
+	variableZeroValue()
+	fmt.Println(aa, ss, kk)
+}
